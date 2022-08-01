002fd0e8990b9104487cfba2b0c95cd506a1c227
Mariya Zhuchina <zhuchina@yandex.ru>
2022-08-01 18:51:33 +0300

    Changed according to the 1st review

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from re import fullmatch
 
 from flask import jsonify, request
@@ -12,8 +13,8 @@ from .utils import get_unique_short_id, is_unique
 def get_url(short_id):
     url_map = URL_map.query.filter_by(short=short_id).first()
     if url_map is None:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
-    return jsonify(url=url_map.original), 200
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND.value)
+    return jsonify(url=url_map.original), HTTPStatus.OK.value
 
 
 @app.route('/api/id/', methods=['POST'])
@@ -44,4 +45,4 @@ def create_id():
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify(url_map.to_dict()), 201
+    return jsonify(url_map.to_dict()), HTTPStatus.CREATED.value
